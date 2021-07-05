35da5f98d9e1ef6abfb76b4ac888094cb6c40eca
kemonsense <qzjaman@gmail.com>
2021-07-05 19:08:12 +0600

    Error Handling & Alerts Done

--- frontend/src/components/layout/Alerts.js
+++ frontend/src/components/layout/Alerts.js
@@ -6,12 +6,24 @@ import PropTypes from "prop-types";
 export class Alerts extends Component {
   static propTypes = {
     error: PropTypes.object.isRequired,
+    message: PropTypes.object.isRequired,
   };
 
   componentDidUpdate(prevProps) {
-    const { error, alert } = this.props;
+    const { error, alert, message } = this.props;
     if (error !== prevProps.error) {
-      alert.error("This is a error");
+      if (error.msg.name) alert.error(`Name: ${error.msg.name.join()}`);
+      if (error.msg.email) alert.error(`Email: ${error.msg.email.join()}`);
+      if (error.msg.message)
+        alert.error(`Message: ${error.msg.message.join()}`);
+      if (error.msg.non_field_errors)
+        alert.error(error.msg.non_field_errors.join());
+      if (error.msg.username) alert.error(error.msg.username.join());
+    }
+
+    if (message !== prevProps.message) {
+      if (message.deleteLead) alert.success(message.deleteLead);
+      if (message.addLead) alert.success(message.addLead);
     }
   }
 
@@ -22,6 +34,7 @@ export class Alerts extends Component {
 
 const mapStateToProps = (state) => ({
   error: state.errors,
+  message: state.messages,
 });
 
 export default connect(mapStateToProps)(withAlert()(Alerts));
